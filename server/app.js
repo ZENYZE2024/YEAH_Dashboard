0cabc71660e16c64e5b79334e8c923dfedeb1a83
akshaysubramanian <akshaysubramanian.zenyze@gmail.com>
2024-09-02 16:34:21 +0530

    latest commit

--- server/app.js
+++ server/app.js
@@ -786,5 +786,5 @@ app.get('*', (req, res) => {
 });
 
 app.listen(process.env.PORT, () => {
-    console.log(`Server is running on http://localhost:${process.env.PORT}`);
+    console.log(`Server is running on https://admin.yeahtrips.in:${process.env.PORT}`);
 });
